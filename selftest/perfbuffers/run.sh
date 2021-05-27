3caa99e84e06229b9a9987c430d3805ee4331130
grantseltzer <grantseltzer@gmail.com>
2021-05-27 18:53:42 +0000

    Add a libbpf submodule for use with the selftests
    This adds a libbpf submodule under selftest. When the selftest run
    script is executed it goes into the libbpf submodule, checks out
    a particular supported version git SHA, and rebuilds the libbpf static
    archive. The selftests then link against that archive and run.
    
    This also updates the self tests themselves to fix an inconsistency
    where they would sometimes timeout if there isn't enough activity on
    the machine it's running on (run mmap a bunch of time to trigger the bpf
    programs).
    
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- selftest/perfbuffers/run.sh
+++ selftest/perfbuffers/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-GREEN='    \033[0;32m'
-RED='    \033[0;31m'
+GREEN='\033[0;32m'
+RED='\033[0;31m'
 NC='\033[0m' 
 
 VERSION_LIMIT=4.3
--- selftest/run.sh
+++ selftest/run.sh
@@ -2,13 +2,34 @@
 
 GREEN='\033[0;32m'
 RED='\033[0;31m'
+YELLOW='\033[0;33m'
 NC='\033[0m' 
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+OUT_DIR=`realpath ./dist`
 
-for d in ${DIR}/*/
+cat .libbpf-versions.txt | while read line 
 do
-    echo -e "${GREEN}[*] RUNNING $d ${NC}"
-    ( cd $d && bash "run.sh" )
-done
+    VERSION=$(echo $line | awk -F" " '{print $1}')
+    SHA=$(echo $line | awk -F" " '{print $2}')
+    echo -e "${YELLOW}Attempting to run selftests with libbpf $VERSION ${NC}"
 
+    # build libbpf version
+    rm -rf dist/*
+    pushd libbpf-module/src &> /dev/null
+    git checkout $SHA &> /dev/null
+    make OBJDIR=$OUT_DIR BUILD_STATIC_ONLY=1
+    popd &> /dev/null
+
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+    for d in ${DIR}/*/
+    do
+        if [[ $(basename $d) == "libbpf-module" ]] || [[ $(basename $d) == "dist" ]]; then
+            continue
+        else
+            echo -e "${GREEN}[*] RUNNING $d ${NC}"
+            ( cd $d && bash "run.sh" )
+            echo
+        fi
+    done
+done
