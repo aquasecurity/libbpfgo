4928d36fafac1c2a3c0337f26eff5db69522eb82
grantseltzer <grantseltzer@gmail.com>
2021-07-08 16:38:34 -0400

    Change BPFMap method `Update` to unsafe.Pointer (#32)
    * Change BPFMap method Update to unsafe.Pointer
    
    This makes a breaking change to the libbpfgo API in which
    the BPFMap.Update() method takes two unsafe.Pointer instead
    of two interface{}. This is so that libbpfgo does not have
    to know or care about the type of the passed value. If bpf
    and libbpf can support the datatype, so should libbpfgo.
    
    It's important to note that it would then be on the user to
    correctly pass arrays/slices (reference must be passed to the
    first element, not the array/slice itself).
    
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>
    
    * Change BPFMap methods DeleteKey and GetValue to unsafe.Pointer
    
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- libbpfgo.go
+++ libbpfgo.go
@@ -472,46 +472,58 @@ func (b *BPFMap) ValueSize() int {
 	return int(C.bpf_map__value_size(b.bpfMap))
 }
 
-func (b *BPFMap) GetValue(key interface{}) ([]byte, error) {
-	keyPtr, err := GetUnsafePointer(key)
-	if err != nil {
-		return nil, fmt.Errorf("failed to lookup value in map %s: unknown key type %T", b.name, key)
-	}
-
+// GetValue takes a pointer to the key which is stored in the map.
+// It returns the associated value as a slice of bytes.
+// All basic types, and structs are supported as keys.
+//
+// NOTE: Slices and arrays are also supported but special care
+// should be taken as to take a reference to the first element
+// in the slice or array instead of the slice/array itself, as to
+// avoid undefined behavior.
+func (b *BPFMap) GetValue(key unsafe.Pointer) ([]byte, error) {
 	value := make([]byte, b.ValueSize())
 	valuePtr := unsafe.Pointer(&value[0])
 
-	errC := C.bpf_map_lookup_elem(b.fd, keyPtr, valuePtr)
+	errC := C.bpf_map_lookup_elem(b.fd, key, valuePtr)
 	if errC != 0 {
 		return nil, fmt.Errorf("failed to lookup value %v in map %s", key, b.name)
 	}
 	return value, nil
 }
 
-func (b *BPFMap) DeleteKey(key interface{}) error {
-	keyPtr, err := GetUnsafePointer(key)
-	if err != nil {
-		return fmt.Errorf("failed to update map %s: unknown key type %T", b.name, key)
-	}
-
-	errC := C.bpf_map_delete_elem(b.fd, keyPtr)
+// DeleteKey takes a pointer to the key which is stored in the map.
+// It removes the key and associated value from the BPFMap.
+// All basic types, and structs are supported as keys.
+//
+// NOTE: Slices and arrays are also supported but special care
+// should be taken as to take a reference to the first element
+// in the slice or array instead of the slice/array itself, as to
+// avoid undefined behavior.
+func (b *BPFMap) DeleteKey(key unsafe.Pointer) error {
+	errC := C.bpf_map_delete_elem(b.fd, key)
 	if errC != 0 {
 		return fmt.Errorf("failed to get lookup key %d from map %s", key, b.name)
 	}
 	return nil
 }
 
-func (b *BPFMap) Update(key, value interface{}) error {
-	keyPtr, err := GetUnsafePointer(key)
-	if err != nil {
-		return fmt.Errorf("failed to update map %s: unknown key type %T", b.name, key)
-	}
-	valuePtr, err := GetUnsafePointer(value)
-	if err != nil {
-		return fmt.Errorf("failed to update map %s: unknown value type %T", b.name, value)
-	}
-
-	errC := C.bpf_map_update_elem(b.fd, keyPtr, valuePtr, C.BPF_ANY)
+// Update takes a pointer to a key and a value to associate it with in
+// the BPFMap. The unsafe.Pointer should be taken on a reference to the
+// underlying datatype. All basic types, and structs are supported
+//
+// NOTE: Slices and arrays are supported but references should be passed
+// to the first element in the slice or array.
+//
+// For example:
+//
+//  key := 1
+//  value := []byte{'a', 'b', 'c'}
+//  keyPtr := unsafe.Pointer(&key)
+//  valuePtr := unsafe.Pointer(&value[0])
+//  bpfmap.Update(keyPtr, valuePtr)
+//
+func (b *BPFMap) Update(key, value unsafe.Pointer) error {
+	errC := C.bpf_map_update_elem(b.fd, key, value, C.BPF_ANY)
 	if errC != 0 {
 		return fmt.Errorf("failed to update map %s", b.name)
 	}
---
+++ selftest/map-update/go.mod
@@ -0,0 +1,7 @@
+module github.com/aquasecurity/libbpfgo/selftest/map-update
+
+go 1.16
+
+require github.com/aquasecurity/libbpfgo v0.1.1
+
+replace github.com/aquasecurity/libbpfgo => ../../
