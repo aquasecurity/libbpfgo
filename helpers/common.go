4dd0441a4a7214e340928921bbe6d1e348559571
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2021-09-02 16:42:19 -0300

    helpers/common: make UnameRelease() a common function

--- helpers/common.go
+++ helpers/common.go
@@ -3,6 +3,8 @@ package helpers
 import (
 	"fmt"
 	"os"
+	"strings"
+	"syscall"
 )
 
 func checkEnvPath(env string) (string, error) {
@@ -15,4 +17,22 @@ func checkEnvPath(env string) (string, error) {
 		return filePath, nil
 	}
 	return "", nil
+}
+
+// UnameRelease gets the version string of the current running kernel
+func UnameRelease() (string, error) {
+	var uname syscall.Utsname
+	if err := syscall.Uname(&uname); err != nil {
+		return "", fmt.Errorf("could not get utsname")
+	}
+
+	var buf [65]byte
+	for i, b := range uname.Release {
+		buf[i] = byte(b)
+	}
+
+	ver := string(buf[:])
+	ver = strings.Trim(ver, "\x00")
+
+	return ver, nil
 }
