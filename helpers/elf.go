746aea86417e07a82b2d8763ef78c6c390cd93d9
Guy Arbitman <guy20495@gmail.com>
2022-02-13 07:57:34 +0200

    helpers: Added dynamic symbol resolver Dynamic symbols are very important to attach u(ret)probe on common libraries such as openssl, libc, etc.

--- helpers/elf.go
+++ helpers/elf.go
@@ -6,19 +6,9 @@ import (
 	"fmt"
 )
 
-// SymbolToOffset attempts to resolve a 'symbol' name in the binary found at
-// 'path' to an offset. The offset can be used for attaching a u(ret)probe
-func SymbolToOffset(path, symbol string) (uint32, error) {
-	f, err := elf.Open(path)
-	if err != nil {
-		return 0, fmt.Errorf("could not open elf file to resolve symbol offset: %w", err)
-	}
-
-	syms, err := f.Symbols()
-	if err != nil {
-		return 0, fmt.Errorf("could not open symbol section to resolve symbol offset: %w", err)
-	}
-
+// symbolToOffset attempts to resolve a 'symbol' name in the given binary to an offset.
+// The offset can be used for attaching a u(ret)probe.
+func symbolToOffset(symbol string, f *elf.File, syms []elf.Symbol) (uint32, error) {
 	sectionsToSearchForSymbol := []*elf.Section{}
 
 	for i := range f.Sections {
@@ -48,5 +38,37 @@ func SymbolToOffset(path, symbol string) (uint32, error) {
 		}
 	}
 
-	return 0, fmt.Errorf("symbol not found")
+	return 0, errors.New("symbol not found")
+}
+
+// SymbolToOffset attempts to resolve a 'symbol' name in the binary found at
+// 'path' to an offset. The offset can be used for attaching a u(ret)probe
+func SymbolToOffset(path, symbol string) (uint32, error) {
+	f, err := elf.Open(path)
+	if err != nil {
+		return 0, fmt.Errorf("could not open elf file to resolve symbol offset: %w", err)
+	}
+
+	syms, err := f.Symbols()
+	if err != nil {
+		return 0, fmt.Errorf("could not open symbol section to resolve symbol offset: %w", err)
+	}
+
+	return symbolToOffset(symbol, f, syms)
+}
+
+// DynamicSymbolToOffset attempts to resolve a dynamic 'symbol' name in the binary found at
+// 'path' to an offset. The offset can be used for attaching a u(ret)probe
+func DynamicSymbolToOffset(path, symbol string) (uint32, error) {
+	f, err := elf.Open(path)
+	if err != nil {
+		return 0, fmt.Errorf("could not open elf file to resolve symbol offset: %w", err)
+	}
+
+	syms, err := f.DynamicSymbols()
+	if err != nil {
+		return 0, fmt.Errorf("could not open dynamic symbol section to resolve symbol offset: %w", err)
+	}
+
+	return symbolToOffset(symbol, f, syms)
 }
