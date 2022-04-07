36d64106ca01d8ffd15ee6e3d68f8f6b28d3f36e
grantseltzer <grantseltzer@gmail.com>
2022-04-07 16:43:16 -0400

    Add selftest for tc
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

---
+++ selftest/tc/main.go
@@ -0,0 +1,87 @@
+package main
+
+import "C"
+
+import (
+	"encoding/binary"
+	"os"
+	"os/exec"
+
+	"fmt"
+
+	bpf "github.com/aquasecurity/libbpfgo"
+)
+
+func main() {
+
+	bpfModule, err := bpf.NewModuleFromFile("main.bpf.o")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+	defer bpfModule.Close()
+
+	bpfModule.ListProgramNames()
+
+	err = bpfModule.BPFLoadObject()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	hook := bpfModule.TcHookInit()
+
+	err = hook.SetInterfaceByName("lo")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	hook.SetAttachPoint(bpf.BPFTcEgress)
+
+	tcProg, err := bpfModule.GetProgram("target")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	var tcOpts bpf.TcOpts
+	tcOpts.ProgFd = int(tcProg.GetFd())
+	err = hook.Attach(&tcOpts)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	eventsChannel := make(chan []byte)
+	rb, err := bpfModule.InitRingBuf("events", eventsChannel)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+
+	rb.Start()
+	numberOfEventsReceived := 0
+	go func() {
+		_, err := exec.Command("ping", "localhost", "-c 10").Output()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(-1)
+		}
+	}()
+recvLoop:
+	for {
+		b := <-eventsChannel
+		if binary.LittleEndian.Uint32(b) != 2021 {
+			fmt.Fprintf(os.Stderr, "invalid data retrieved\n")
+			os.Exit(-1)
+		}
+		numberOfEventsReceived++
+		if numberOfEventsReceived > 5 {
+			break recvLoop
+		}
+	}
+
+	rb.Stop()
+	rb.Close()
+}
