9f0408e53407783904d7ef7eb02828f318f4aa04
grantseltzer <grantseltzer@gmail.com>
2022-05-06 09:31:29 -0400

    fix allignment
    Signed-off-by: grantseltzer <grantseltzer@gmail.com>

--- selftest/probe-features/main.bpf.c
+++ selftest/probe-features/main.bpf.c
@@ -6,7 +6,7 @@
 SEC("kprobe/sys_mmap")
 int kprobe__sys_mmap(struct pt_regs *ctx)
 {
-	bpf_printk("Yankees will win the 2022 world series");
+    bpf_printk("Yankees will win the 2022 world series");
     return 0;
 }
 
