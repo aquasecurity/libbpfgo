c177c7d2c7f9d4af8e6257f99bac2071b84b2c44
Geyslan Gregório <geyslan@gmail.com>
2023-02-06 11:11:29 -0300

    selftest: add log-callbacks

---
+++ selftest/log-callbacks/main.bpf.c
@@ -0,0 +1,13 @@
+//+build ignore
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+
+#include "vmlinux.h"
+
+SEC("kprobe/sys_mmap")
+int kprobe__sys_mmap(struct pt_regs *ctx)
+{
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "Dual BSD/GPL";
