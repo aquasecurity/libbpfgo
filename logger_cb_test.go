fda8fb2c4147f9e4aeb7f19df31d1531e37d1ed9
Geyslan Gregório <geyslan@gmail.com>
2023-02-22 15:11:12 -0300

    logger_cb: remove default callbacks
    The logic related to filtering by callbacks must be a decision of the
    library consumers, as it is not possible to know which is the best
    strategy for each case.
    
    This also adds to the related selftest other example that shows how to
    filter using callbacks.

--- logger_cb_test.go
+++ logger_cb_test.go
@@ -1,43 +1,52 @@
 package libbpfgo
 
-import "testing"
+import (
+	"bytes"
+	"io"
+	"os"
+	"testing"
 
-func TestLogFilterOutput(t *testing.T) {
-	tests := []struct {
-		libbpfPrintLevel int
-		output           string
-		expectedResult   bool
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestLogFallback(t *testing.T) {
+	tt := []struct {
+		message string
 	}{
 		{
-			output:         "libbpf: prog 'trace_check_map_func_compatibility': failed to create kprobe 'check_map_func_compatibility+0x0' perf event: No such file or directory\n",
-			expectedResult: true,
+			message: "This is a warning message",
 		},
 		{
-			output:         "libbpf: Kernel error message: Exclusivity flag on\n",
-			expectedResult: true,
+			message: "This is a information message",
 		},
 		{
-			output:         "libbpf: prog 'cgroup_skb_ingress': failed to attach to cgroup 'cgroup': Invalid argument\n",
-			expectedResult: true,
-		},
-		{
-			output:         "libbpf: prog 'cgroup_skb_egress': failed to attach to cgroup 'cgroup': Invalid argument\n",
-			expectedResult: true,
-		},
-		{
-			output:         "This is not a log message that should be filtered\n",
-			expectedResult: false,
-		},
-		{
-			output:         "libbpf: This is not a log message that should be filtered\n",
-			expectedResult: false,
+			message: "This is a debug message",
 		},
 	}
 
-	for _, test := range tests {
-		result := LogFilterOutput(test.libbpfPrintLevel, test.output)
-		if result != test.expectedResult {
-			t.Errorf("For input '%s', expected %v but got %v", test.output, test.expectedResult, result)
-		}
+	for _, tc := range tt {
+		var buf bytes.Buffer
+
+		r, w, err := os.Pipe()
+		require.NoError(t, err, "failed to create pipe")
+
+		writeEnd := os.NewFile(uintptr(w.Fd()), "pipe")
+
+		oldStderr := os.Stderr
+		os.Stderr = writeEnd
+
+		// level is ignored
+		logFallback(LibbpfInfoLevel, tc.message)
+
+		os.Stderr = oldStderr
+
+		err = writeEnd.Close()
+		require.NoError(t, err, "failed to close writeEnd")
+		_, err = io.Copy(&buf, r)
+		require.NoError(t, err, "failed to copy from read end to buffer")
+
+		// The message should be printed to stderr with a newline
+		assert.Equal(t, tc.message+"\n", buf.String())
 	}
 }
--- selftest/log-callbacks/main.go
+++ selftest/log-callbacks/main.go
@@ -13,17 +13,20 @@ import (
 
 var logOutput []string
 
-func log(level int, msg string, keyValues ...interface{}) {
+// log is a handler to save the log output
+func log(level int, msg string) {
 	logOutput = append(logOutput, msg)
 }
 
 func main() {
+	//
+	// Filter example 1: filter out all outputs but containing "found program 'kprobe__sys_mmap'"
+	//
 	filterMatch := "found program 'kprobe__sys_mmap'"
 	bpf.SetLoggerCbs(bpf.Callbacks{
-		Log: log, // use log() as a handler for libbpf outputs that are not excluded by LogFilters
+		Log: log,
 		LogFilters: []func(libLevel int, msg string) bool{
 			func(libLevel int, msg string) bool {
-				// filter all output but containing "found program 'kprobe__sys_mmap'"
 				return !strings.Contains(msg, filterMatch)
 			},
 		},
@@ -34,10 +37,38 @@ func main() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
 	}
-	defer bpfModule.Close()
+	bpfModule.Close()
 
 	if len(logOutput) != 1 {
 		fmt.Fprintln(os.Stderr, fmt.Sprintf("Log output should contain only one output matching the string: %s", filterMatch))
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Log output: %v", logOutput))
+		os.Exit(-1)
+	}
+
+	// clean logOutput
+	logOutput = []string{}
+
+	//
+	// Filter example 2: filter out all outputs which level is LibbpfDebugLevel
+	//
+	bpf.SetLoggerCbs(bpf.Callbacks{
+		Log: log,
+		LogFilters: []func(libLevel int, msg string) bool{
+			func(libLevel int, msg string) bool {
+				return libLevel == bpf.LibbpfDebugLevel
+			},
+		},
+	})
+
+	bpfModule, err = bpf.NewModuleFromFile("main.bpf.o")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(-1)
+	}
+	bpfModule.Close()
+
+	if len(logOutput) != 0 {
+		fmt.Fprintln(os.Stderr, "Log output should be empty")
 		fmt.Fprintln(os.Stderr, fmt.Sprintf("Log output: %v", logOutput))
 		os.Exit(-1)
 	}
