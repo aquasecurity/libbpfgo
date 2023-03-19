f17cb5b109d3f37ed959c71727884f17afa6fcec
Huang Huang <mozillazg101@gmail.com>
2023-03-20 01:13:10 +0800

    Add `AttachIter` to wrap `bpf_program__attach_iter` (#254)
    * Add `AttachIter` to wrap `bpf_program__attach_iter`

--- selftest/common/vmlinux.h
+++ selftest/common/vmlinux.h
@@ -1,6 +1,8 @@
 #ifndef __VMLINUX_H__
 #define __VMLINUX_H__
 
+#pragma clang attribute push(__attribute__((preserve_access_index)), apply_to = record)
+
 typedef __u8 u8;
 
 typedef __s16 s16;
@@ -15,6 +17,10 @@ typedef __s64 s64;
 
 typedef __u64 u64;
 
+typedef int __kernel_pid_t;
+
+typedef __kernel_pid_t pid_t;
+
 struct trace_entry {
 	short unsigned int type;
 	unsigned char flags;
@@ -28,5 +34,27 @@ struct trace_event_raw_sys_enter {
 	long unsigned int args[6];
 	char __data[0];
 };
+
+struct bpf_iter_meta {
+    struct seq_file *seq;
+    __u64 session_id;
+    __u64 seq_num;
+} __attribute__((preserve_access_index));
+
+struct bpf_iter__task {
+    struct bpf_iter_meta *meta;
+    struct task_struct *task;
+} __attribute__((preserve_access_index));
+
+struct seq_file;
+
+struct task_struct {
+    pid_t pid;
+    pid_t tgid;
+    struct task_struct *parent;
+    char comm[16];
+};
+
+#pragma clang attribute pop
 
 #endif /* __VMLINUX_H__ */
---
+++ selftest/iter/main.bpf.c
@@ -0,0 +1,20 @@
+//+build ignore
+#include <linux/bpf.h>
+#include <bpf/bpf_helpers.h>
+#include <bpf/bpf_tracing.h>
+
+#include "vmlinux.h"
+
+SEC("iter/task")
+int iter__task(struct bpf_iter__task *ctx)
+{
+    struct seq_file *seq = ctx->meta->seq;
+    struct task_struct *task = ctx->task;
+    if (task == NULL)
+        return 0;
+
+    BPF_SEQ_PRINTF(seq, "%d\t%d\t%s\n", task->parent->pid, task->pid, task->comm);
+    return 0;
+}
+
+char LICENSE[] SEC("license") = "GPL";
