7da958042196bb11a59a0b1adcc4b5ccd67a7b51
Geyslan Gregório <geyslan@gmail.com>
2023-08-02 12:00:47 -0300

    chore: increase timeout for selftests (#349)
    All selftests are now running with a timeout of 10 seconds, except for
    attachgenericfd, which has now its own run.sh with a timeout of
    15 seconds.

--- selftest/uprobe/run.sh
+++ selftest/uprobe/run.sh
@@ -2,8 +2,8 @@
 
 # SETTINGS
 
-TEST=$(dirname $0)/$1	# execute
-TIMEOUT=5			# seconds
+TEST=$(dirname $0)/$1  # execute
+TIMEOUT=10             # seconds
 
 CTEST=$(dirname $0)/ctest
 GOTEST=$(dirname $0)/gotest
