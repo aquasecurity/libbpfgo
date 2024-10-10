6452f22c18c1a330fddc23e4a59c255f121dcde5
Geyslan Gregório <geyslan@gmail.com>
2024-10-10 09:04:58 -0300

    chore: deprecate helpers pkg

--- helpers/btfhub.go
+++ helpers/btfhub.go
@@ -1,1 +1,3 @@
+// Deprecated: helpers package is deprecated and will be removed.
+// See https://github.com/aquasecurity/tracee/pull/4090
 package helpers
--- selftest/uprobe/main.go
+++ selftest/uprobe/main.go
@@ -2,30 +2,14 @@ package main
 
 import "C"
 import (
+	"debug/elf"
 	"encoding/binary"
+	"errors"
 	"fmt"
 	"os"
 
 	bpf "github.com/aquasecurity/libbpfgo"
-	"github.com/aquasecurity/libbpfgo/helpers"
 )
-
-func resizeMap(module *bpf.Module, name string, size uint32) error {
-	m, err := module.GetMap("events")
-	if err != nil {
-		return err
-	}
-
-	if err = m.Resize(size); err != nil {
-		return err
-	}
-
-	if actual := m.GetMaxEntries(); actual != size {
-		return fmt.Errorf("map resize failed, expected %v, actual %v", size, actual)
-	}
-
-	return nil
-}
 
 func main() {
 	if len(os.Args) < 3 {
@@ -61,7 +45,7 @@ func main() {
 		os.Exit(-1)
 	}
 
-	offset, err := helpers.SymbolToOffset(binaryPath, symbolName)
+	offset, err := symbolToOffset(binaryPath, symbolName)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(-1)
@@ -103,4 +87,74 @@ recvLoop:
 	rb.Close()
 	rb.Close()
 	rb.Stop()
+}
+
+// symbolToOffset attempts to resolve a 'symbol' name in the binary found at
+// 'path' to an offset. The offset can be used for attaching a u(ret)probe
+func symbolToOffset(path, symbol string) (uint32, error) {
+	f, err := elf.Open(path)
+	if err != nil {
+		return 0, fmt.Errorf("could not open elf file to resolve symbol offset: %w", err)
+	}
+	defer f.Close()
+
+	regularSymbols, regularSymbolsErr := f.Symbols()
+	dynamicSymbols, dynamicSymbolsErr := f.DynamicSymbols()
+
+	// Only if we failed getting both regular and dynamic symbols - then we abort.
+	if regularSymbolsErr != nil && dynamicSymbolsErr != nil {
+		return 0, fmt.Errorf("could not open regular or dynamic symbol sections to resolve symbol offset: %w %s", regularSymbolsErr, dynamicSymbolsErr)
+	}
+
+	// Concatenating into a single list.
+	// The list can have duplications, but we will find the first occurrence which is sufficient.
+	syms := append(regularSymbols, dynamicSymbols...)
+
+	sectionsToSearchForSymbol := []*elf.Section{}
+
+	for i := range f.Sections {
+		if f.Sections[i].Flags == elf.SHF_ALLOC+elf.SHF_EXECINSTR {
+			sectionsToSearchForSymbol = append(sectionsToSearchForSymbol, f.Sections[i])
+		}
+	}
+
+	var executableSection *elf.Section
+
+	for j := range syms {
+		if syms[j].Name == symbol {
+			// Find what section the symbol is in by checking the executable section's
+			// addr space.
+			for m := range sectionsToSearchForSymbol {
+				if syms[j].Value > sectionsToSearchForSymbol[m].Addr &&
+					syms[j].Value < sectionsToSearchForSymbol[m].Addr+sectionsToSearchForSymbol[m].Size {
+					executableSection = sectionsToSearchForSymbol[m]
+				}
+			}
+
+			if executableSection == nil {
+				return 0, errors.New("could not find symbol in executable sections of binary")
+			}
+
+			return uint32(syms[j].Value - executableSection.Addr + executableSection.Offset), nil
+		}
+	}
+
+	return 0, fmt.Errorf("symbol %s not found in %s", symbol, path)
+}
+
+func resizeMap(module *bpf.Module, name string, size uint32) error {
+	m, err := module.GetMap("events")
+	if err != nil {
+		return err
+	}
+
+	if err = m.SetMaxEntries(size); err != nil {
+		return err
+	}
+
+	if actual := m.MaxEntries(); actual != size {
+		return fmt.Errorf("map resize failed, expected %v, actual %v", size, actual)
+	}
+
+	return nil
 }
