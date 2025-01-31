69615c2e5d24963a7217de10320b06e03c404efc
Ian Chen <ychen.cs10@nycu.edu.tw>
2025-02-01 00:43:51 +0800

    fix: clang-format check

--- selftest/struct-ops/main.bpf.c
+++ selftest/struct-ops/main.bpf.c
@@ -39,113 +39,112 @@ UEI_DEFINE(uei);
 #define SHARED_DSQ 0
 
 struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__uint(key_size, sizeof(u32));
-	__uint(value_size, sizeof(u64));
-	__uint(max_entries, 2);			/* [local, global] */
+    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+    __uint(key_size, sizeof(u32));
+    __uint(value_size, sizeof(u64));
+    __uint(max_entries, 2); /* [local, global] */
 } stats SEC(".maps");
 
 static void stat_inc(u32 idx)
 {
-	u64 *cnt_p = bpf_map_lookup_elem(&stats, &idx);
-	if (cnt_p)
-		(*cnt_p)++;
+    u64 *cnt_p = bpf_map_lookup_elem(&stats, &idx);
+    if (cnt_p)
+        (*cnt_p)++;
 }
 
 s32 BPF_STRUCT_OPS(simple_select_cpu, struct task_struct *p, s32 prev_cpu, u64 wake_flags)
 {
-	bool is_idle = false;
-	s32 cpu;
+    bool is_idle = false;
+    s32 cpu;
 
-	cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &is_idle);
-	if (is_idle) {
-		stat_inc(0);	/* count local queueing */
-		scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0);
-	}
+    cpu = scx_bpf_select_cpu_dfl(p, prev_cpu, wake_flags, &is_idle);
+    if (is_idle) {
+        stat_inc(0); /* count local queueing */
+        scx_bpf_dsq_insert(p, SCX_DSQ_LOCAL, SCX_SLICE_DFL, 0);
+    }
 
-	return cpu;
+    return cpu;
 }
 
 void BPF_STRUCT_OPS(simple_enqueue, struct task_struct *p, u64 enq_flags)
 {
-	stat_inc(1);	/* count global queueing */
+    stat_inc(1); /* count global queueing */
 
-	if (fifo_sched) {
-		scx_bpf_dsq_insert(p, SHARED_DSQ, SCX_SLICE_DFL, enq_flags);
-	} else {
-		u64 vtime = p->scx.dsq_vtime;
+    if (fifo_sched) {
+        scx_bpf_dsq_insert(p, SHARED_DSQ, SCX_SLICE_DFL, enq_flags);
+    } else {
+        u64 vtime = p->scx.dsq_vtime;
 
-		/*
-		 * Limit the amount of budget that an idling task can accumulate
-		 * to one slice.
-		 */
-		if (time_before(vtime, vtime_now - SCX_SLICE_DFL))
-			vtime = vtime_now - SCX_SLICE_DFL;
+        /*
+         * Limit the amount of budget that an idling task can accumulate
+         * to one slice.
+         */
+        if (time_before(vtime, vtime_now - SCX_SLICE_DFL))
+            vtime = vtime_now - SCX_SLICE_DFL;
 
-		scx_bpf_dsq_insert_vtime(p, SHARED_DSQ, SCX_SLICE_DFL, vtime,
-					 enq_flags);
-	}
+        scx_bpf_dsq_insert_vtime(p, SHARED_DSQ, SCX_SLICE_DFL, vtime, enq_flags);
+    }
 }
 
 void BPF_STRUCT_OPS(simple_dispatch, s32 cpu, struct task_struct *prev)
 {
-	scx_bpf_dsq_move_to_local(SHARED_DSQ);
+    scx_bpf_dsq_move_to_local(SHARED_DSQ);
 }
 
 void BPF_STRUCT_OPS(simple_running, struct task_struct *p)
 {
-	if (fifo_sched)
-		return;
+    if (fifo_sched)
+        return;
 
-	/*
-	 * Global vtime always progresses forward as tasks start executing. The
-	 * test and update can be performed concurrently from multiple CPUs and
-	 * thus racy. Any error should be contained and temporary. Let's just
-	 * live with it.
-	 */
-	if (time_before(vtime_now, p->scx.dsq_vtime))
-		vtime_now = p->scx.dsq_vtime;
+    /*
+     * Global vtime always progresses forward as tasks start executing. The
+     * test and update can be performed concurrently from multiple CPUs and
+     * thus racy. Any error should be contained and temporary. Let's just
+     * live with it.
+     */
+    if (time_before(vtime_now, p->scx.dsq_vtime))
+        vtime_now = p->scx.dsq_vtime;
 }
 
 void BPF_STRUCT_OPS(simple_stopping, struct task_struct *p, bool runnable)
 {
-	if (fifo_sched)
-		return;
+    if (fifo_sched)
+        return;
 
-	/*
-	 * Scale the execution time by the inverse of the weight and charge.
-	 *
-	 * Note that the default yield implementation yields by setting
-	 * @p->scx.slice to zero and the following would treat the yielding task
-	 * as if it has consumed all its slice. If this penalizes yielding tasks
-	 * too much, determine the execution time by taking explicit timestamps
-	 * instead of depending on @p->scx.slice.
-	 */
-	p->scx.dsq_vtime += (SCX_SLICE_DFL - p->scx.slice) * 100 / p->scx.weight;
+    /*
+     * Scale the execution time by the inverse of the weight and charge.
+     *
+     * Note that the default yield implementation yields by setting
+     * @p->scx.slice to zero and the following would treat the yielding task
+     * as if it has consumed all its slice. If this penalizes yielding tasks
+     * too much, determine the execution time by taking explicit timestamps
+     * instead of depending on @p->scx.slice.
+     */
+    p->scx.dsq_vtime += (SCX_SLICE_DFL - p->scx.slice) * 100 / p->scx.weight;
 }
 
 void BPF_STRUCT_OPS(simple_enable, struct task_struct *p)
 {
-	p->scx.dsq_vtime = vtime_now;
+    p->scx.dsq_vtime = vtime_now;
 }
 
 s32 BPF_STRUCT_OPS_SLEEPABLE(simple_init)
 {
-	return scx_bpf_create_dsq(SHARED_DSQ, -1);
+    return scx_bpf_create_dsq(SHARED_DSQ, -1);
 }
 
 void BPF_STRUCT_OPS(simple_exit, struct scx_exit_info *ei)
 {
-	UEI_RECORD(uei, ei);
+    UEI_RECORD(uei, ei);
 }
 
 SCX_OPS_DEFINE(simple_ops,
-	       .select_cpu		= (void *)simple_select_cpu,
-	       .enqueue			= (void *)simple_enqueue,
-	       .dispatch		= (void *)simple_dispatch,
-	       .running			= (void *)simple_running,
-	       .stopping		= (void *)simple_stopping,
-	       .enable			= (void *)simple_enable,
-	       .init			= (void *)simple_init,
-	       .exit			= (void *)simple_exit,
-	       .name			= "simple");
+               .select_cpu = (void *) simple_select_cpu,
+               .enqueue = (void *) simple_enqueue,
+               .dispatch = (void *) simple_dispatch,
+               .running = (void *) simple_running,
+               .stopping = (void *) simple_stopping,
+               .enable = (void *) simple_enable,
+               .init = (void *) simple_init,
+               .exit = (void *) simple_exit,
+               .name = "simple");
