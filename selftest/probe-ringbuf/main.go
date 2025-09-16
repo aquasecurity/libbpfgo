94a57446b3d2ee63badaf2b5bab45d1f0e83eea0
Geyslan Gregório <geyslan@gmail.com>
2025-09-16 17:15:07 -0300

    chore: add GetKernelVersion and GetKernelRelease ...
    ... to provide more information to the selftests.

--- selftest/probe-ringbuf/main.go
+++ selftest/probe-ringbuf/main.go
@@ -3,11 +3,19 @@ package main
 import "C"
 
 import (
+	"errors"
+	"log"
+
 	bpf "github.com/aquasecurity/libbpfgo"
 	"github.com/aquasecurity/libbpfgo/selftest/common"
 )
 
 func main() {
+	kernelVersion := common.GetKernelVersion()
+	kernelRelease := common.GetKernelRelease()
+	log.Printf("Running on kernel: %s", kernelVersion)
+	log.Printf("Kernel release: %s", kernelRelease)
+
 	bpfModule, err := bpf.NewModuleFromFile("main.bpf.o")
 	if err != nil {
 		common.Error(err)
@@ -21,9 +29,17 @@ func main() {
 
 	// Should be supported from 5.8 onwards
 	isSupported, err := bpf.BPFMapTypeIsSupported(bpf.MapTypeRingbuf)
-	if err != nil || !isSupported {
+
+	log.Printf("BPFMapTypeIsSupported(MapTypeRingbuf) returned: isSupported=%v, err=%v", isSupported, err)
+
+	if err != nil {
 		common.Error(err)
 	}
+	if !isSupported {
+		common.Error(errors.New("ringbuf is not supported on this kernel (expected supported from 5.8 onwards)"))
+	}
+
+	log.Println("Ringbuf is supported as expected")
 
 	eventsChannel1 := make(chan []byte)
 	_, err = bpfModule.InitRingBuf("events1", eventsChannel1)
